2d9be3f549e5830251a867ccb9fd1f0dd33d5f88
Scott Perry <Scott.Perry@health.nsw.gov.au>
2023-04-17 10:07:46 +1000

    WIP

--- index.php
+++ index.php
@@ -17,6 +17,8 @@
 
 echo "Hello World!";
 
+$requestUri = "GET https://slhdb2c.b2clogin.com/slhdb2c.onmicrosoft.com/B2C_1_SignUpAndSignInHealthyBeginnings/oauth2/v2.0/authorize?client_id=104a6d38-e647-4733-8c39-51e8376fede5&response_type=code&redirect_uri=https%3A%2F%2Fas-healthy-beginnings.azurewebsites.net%2F.auth%2Flogin%2Fslhdb2c%2Fcallback&response_mode=query&scope=offline_access%20https://slhdb2c.onmicrosoft.com/104a6d38-e647-4733-8c39-51e8376fede5/access_as_user"
+
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_URL, $requestUri);
