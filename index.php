bd79e05c988ceae8124c00568b37a4c09f84244e
Scott Perry <Scott.Perry@health.nsw.gov.au>
2023-04-18 11:39:27 +1000

    WIP

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ https://learn.microsoft.com/en-us/azure/active-directory-b2c/authorization-code-
 // See:
 https://www.twilio.com/blog/5-ways-to-make-http-requests-in-php
 
-echo "Hello World!";
+echo "Hello World! - V1";
 
 $requestUri = "GET https://slhdb2c.b2clogin.com/slhdb2c.onmicrosoft.com/B2C_1_SignUpAndSignInHealthyBeginnings/oauth2/v2.0/authorize?client_id=104a6d38-e647-4733-8c39-51e8376fede5&response_type=code&redirect_uri=https%3A%2F%2Fas-healthy-beginnings.azurewebsites.net%2F.auth%2Flogin%2Fslhdb2c%2Fcallback&response_mode=query&scope=offline_access%20https://slhdb2c.onmicrosoft.com/104a6d38-e647-4733-8c39-51e8376fede5/access_as_user"
 
